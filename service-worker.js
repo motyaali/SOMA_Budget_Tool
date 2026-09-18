9c7992016f285c36e89376497010951e929acd85
Motya Ali <motyaali@pm.me>
2026-09-18 03:51:15 -0700

    Elevate service-worker.js for version 2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "soma-budget-v1";
+const CACHE = "soma-budget-v2";
 const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icons/icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
